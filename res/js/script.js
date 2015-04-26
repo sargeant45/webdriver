1f894ba2812f1455317b398ec2f4e483ad74dfba
sargeant45 <ethanarterberry@gmail.com>
2015-04-25 22:05:36 -0700

    Add the title and the description, plus some tweaks

--- res/js/script.js
+++ res/js/script.js
@@ -6,7 +6,19 @@ Open-source under the Unlicense.
 */
 
 function loopGenerate() {
-    setInterval(function(){rectangulate();}, 1000);
+    var title = randomString(8);
+    document.title = title;
+    setInterval(function(){rectangulate()}, 1000);
+}
+
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+    	var randomPoz = Math.floor(Math.random() * charSet.length);
+    	randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
 }
 
 function rectangulate() {
